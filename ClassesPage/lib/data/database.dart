7af4a7a07f517f7d7cf2163dc7cb06211052c244
corndog-overflow <kabirs@bu.edu>
2022-12-09 01:30:13 -0500

    class reg page, updated yaml. needs to be integrated

---
+++ ClassesPage/lib/data/database.dart
@@ -0,0 +1,26 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+class ToDoDataBase {
+  List toDoList = [];
+
+  // reference our box
+  final _myBox = Hive.box('mybox');
+
+  // run this method if this is the 1st time ever opening this app
+  void createInitialData() {
+    toDoList = [
+      ["EC419 B2", false],
+      ["EC330 A2", false],
+    ];
+  }
+
+  // load the data from database
+  void loadData() {
+    toDoList = _myBox.get("TODOLIST");
+  }
+
+  // update the database
+  void updateDataBase() {
+    _myBox.put("TODOLIST", toDoList);
+  }
+}
